c76b79ba88496ecff1194a63e0a63e4826077e2d
NikichXP <nikichx2@gmail.com>
2014-08-15 01:04:16 +0300

    1.1.1
    Небольшие правки.

--- src/com/nikichxp/firstapp/MainActivity.java
+++ src/com/nikichxp/firstapp/MainActivity.java
@@ -9,18 +9,11 @@ import android.widget.*;
 
 public class MainActivity extends ActionBarActivity {
 
-	private static TextView textMessage;
-	private static Button pushButton;
-	private static Button resetButton;
-	private static CountDownTimer countdownToHideResetButton, pushButtonBlockCountdown;
-	
-	private static int countOfPushButtonPressed = 0;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -32,19 +25,42 @@ public class MainActivity extends ActionBarActivity {
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
+		private static TextView textMessage;	//Сообщение, которое видит пользователь
+		private static Button pushButton;		//Кнопка push. Приносит боль и негодование у программы.
+		private static Button resetButton;		//Кнопка reset. Новая игра :D
+		private static CountDownTimer countdownTimer;
+		private static int countOfPushButtonPressed = 0;
+
 		public PlaceholderFragment() {
 		}
 
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			
-			final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+
+			final View rootView = inflater.inflate(R.layout.fragment_main,
+					container, false);
+
 			textMessage = (TextView) rootView.findViewById(R.id.msgText);
+			resetButton = (Button) rootView.findViewById(R.id.resetBtn);
 			pushButton = (Button) rootView.findViewById(R.id.pushBtn);
-			pushButton.setOnClickListener(new OnClickListener() {				
+
+			resetButton.setOnClickListener(new OnClickListener() {
+
 				@Override
 				public void onClick(View v) {
-					textMessage = (TextView) rootView.findViewById(R.id.msgText);
+					textMessage = (TextView) rootView
+							.findViewById(R.id.msgText);
+					textMessage.setText(R.string.hello_world);
+					countOfPushButtonPressed = 0;
+				}
+			});
+
+			pushButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					textMessage = (TextView) rootView
+							.findViewById(R.id.msgText);
 					countOfPushButtonPressed++;
 					switch (countOfPushButtonPressed) {
 					case 1:
@@ -64,46 +80,43 @@ public class MainActivity extends ActionBarActivity {
 						break;
 					case 20:
 						textMessage.setText(R.string.textAfter20Press);
-						countdownToHideResetButton = new CountDownTimer(30000, 1000) {
+						countdownTimer = new CountDownTimer(30000,
+								1000) {
 							@Override
 							public void onTick(long millisUntilFinished) {
-								
+
 							}
+
 							@Override
 							public void onFinish() {
-								Button reset = (Button) rootView.findViewById(R.id.resetBtn);
+								Button reset = (Button) rootView
+										.findViewById(R.id.resetBtn);
 								reset.setVisibility(View.INVISIBLE);
 							}
 						};
-						countdownToHideResetButton.start();
+						countdownTimer.start();
 						break;
 					default:
 						break;
 					}
 					pushButton.setVisibility(View.INVISIBLE);
-					Toast.makeText(rootView.getContext(), R.string.buttonIsInactive, Toast.LENGTH_SHORT).show();
-					pushButtonBlockCountdown = new CountDownTimer(5000, 1000) {
+					Toast.makeText(rootView.getContext(),
+							R.string.buttonIsInactive, Toast.LENGTH_SHORT)
+							.show();
+					countdownTimer = new CountDownTimer(5000, 1000) {
 						@Override
 						public void onTick(long millisUntilFinished) {
 						}
+
 						@Override
 						public void onFinish() {
 							pushButton.setVisibility(View.VISIBLE);
 						}
 					};
-					pushButtonBlockCountdown.start();
+					countdownTimer.start();
 				}
 			});
-			resetButton = (Button) rootView.findViewById(R.id.resetBtn);
-			resetButton.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					textMessage = (TextView) rootView.findViewById(R.id.msgText);
-					textMessage.setText(R.string.hello_world);
-					countOfPushButtonPressed = 0;
-				}
-			});
+
 			return rootView;
 		}
 	}
